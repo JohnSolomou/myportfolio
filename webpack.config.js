18cc5c440a6dce3cb71bc917a9ab8be6674cf985
John Solomou <solomoufam@yahoo.com>
2022-07-14 09:42:53 -0400

    fixed enviornment webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const path = require("path");
+const { DefinePlugin } = require("webpack");
+const { config } = require("dotenv");
 const Dotenv = require("dotenv-webpack");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const env = config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+
 module.exports = {
   mode: "development",
   devServer: {
@@ -27,15 +35,17 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({ template: "./public/index.html" }),
     new MiniCssExtractPlugin(),
-    new Dotenv({
-      path: "./some.other.env", // load this now instead of the ones in '.env'
-      safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
-      allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
-      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
-      silent: true, // hide any errors
-      defaults: false, // load '.env.defaults' as the default values if empty.
-      prefix: "import.meta.env.", // reference your env variables as 'import.meta.env.ENV_VAR'.
-    }),
+    new Dotenv(),
+    new DefinePlugin(envKeys),
+    // new Dotenv({
+    //   path: "./.env", // load this now instead of the ones in '.env'
+    //   safe: true, // load '.env.example' to verify the '.env' variables are all set. Can also be a string to a different file.
+    //   allowEmptyValues: true, // allow empty variables (e.g. `FOO=`) (treat it as empty string, rather than missing)
+    //   systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+    //   silent: true, // hide any errors
+    //   defaults: false, // load '.env.defaults' as the default values if empty.
+    //   prefix: "import.meta.env.", // reference your env variables as 'import.meta.env.ENV_VAR'.
+    // }),
   ],
 
   resolve: {
