2981529247e18e215393b0a991c5c1e473fc0a89
John Solomou <solomoufam@yahoo.com>
2022-08-17 08:44:27 -0400

    fixed routing problem. on refresh would not load page. switched to HashRouter instead of BrouserRouter fixed the problem

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import "./App.css";
 import Hero from "./Hero";
 import React from "react";
-// import Navigation from "./Nav";
+import Navigation from "./Nav";
 import About from "./About";
 
 import Work from "./Work";
 import Footer from "./Footer";
-import Layout from "./components/Layout";
+// import Layout from "./components/Layout";
 import Contact from "./Contact";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// function App() {
-//   return (
-//     <div className="dark-hero ">
-//       <Navigation />
-//       <Hero />
-//       <About />
-//       <Work />
-//       <Contact />
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
-//       <Footer></Footer>
-//     </div>
-//   );
-// }
-
-// export default App;
 function App() {
   return (
     <div>
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Hero />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/work" element={<Work />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Layout>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Work />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,6 @@ import { Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { LinkContainer } from "react-router-bootstrap";
-
 function Navigation() {
   return (
     <Navbar
@@ -30,77 +29,13 @@ function Navigation() {
             <LinkContainer to="/work">
               <Nav.Link>Work</Nav.Link>
             </LinkContainer>
-            <LinkContainer to="contact">
+            <LinkContainer to="/contact">
               <Nav.Link>contact</Nav.Link>
             </LinkContainer>
           </Nav>
         </div>
       </Navbar.Collapse>
     </Navbar>
-
-    // function Navigation() {
-    //   return (
-    //     <Navbar
-    //       collapseOnSelect
-    //       expand="lg"
-    //       bg="dark-hero"
-    //       variant="dark"
-    //       className="center  sticky-top"
-    //     >
-    //       <Navbar.Toggle aria-controls="responsive-navbar-nav " className="m-2 " />
-    //       <Navbar.Collapse
-    //         className="  justify-content-end "
-    //         id="responsive-navbar-nav"
-    //       >
-    //         <div className="brackets ">
-    //           <Nav className="ml-auto">
-    //             <LinkContainer >About</LinkContainer>
-    //             <LinkContainer >Work</LinkContainer>
-    //             <LinkContainer >contact</LinkContainer>
-    //           </Nav>
-    //         </div>
-    //       </Navbar.Collapse>
-    //     </Navbar>
-
-    // <div>
-    //   <nav id="home" className="navbar navbar-expand-md navbar-dark dark-hero">
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-toggle="collapse"
-    //       data-target="#navbar-collapse"
-    //       aria-controls="navbarNav"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div
-    //       className="collapse navbar-collapse flex-row-reverse"
-    //       id="navbar-collapse"
-    //     >
-    //       <div className="brackets">
-    //         <ul className="navbar-nav">
-    //           <li className="nav-item">
-    //             <a className="nav-link" href="#about">
-    //               about
-    //             </a>
-    //           </li>
-    //           <li className="nav-item">
-    //             <a className="nav-link" href="#work">
-    //               work
-    //             </a>
-    //           </li>
-    //           <li className="nav-item">
-    //             <a className="nav-link" href="#contact">
-    //               contact
-    //             </a>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
   );
 }
 export default Navigation;
